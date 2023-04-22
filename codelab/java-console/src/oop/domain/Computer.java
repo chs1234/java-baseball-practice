200b7895e95d3375c4b07c94a8fcc207c208301b
chs1234 <chs4413@gmail.com>
2023-04-22 12:49:00 +0900

    feat: 유저/컴퓨터 도메인 생성

--- codelab/java-console/src/oop/domain/Computer.java
+++ codelab/java-console/src/oop/domain/Computer.java
@@ -1,5 +1,38 @@
 package oop.domain;
 
-public class Computer {
+import oop.util.RandomNumberGenerator;
 
+public class Computer {
+    private int ballCnt;
+    private int strikeCnt;
+    private int[] randomNumbers;
+
+    public void clearCnt() {
+        ballCnt = 0;
+        strikeCnt = 0;
+    }
+
+    public void createRandomNumbers() {
+        randomNumbers = new RandomNumberGenerator().createRandomNumbers();
+    }
+
+    public int getBallCnt() {
+        return ballCnt;
+    }
+
+    public int getStrikeCnt() {
+        return strikeCnt;
+    }
+
+    public int[] getRandomNumbers() {
+        return randomNumbers;
+    }
+
+    public void increaseBallCnt() {
+        ballCnt++;
+    }
+
+    public void increaseStrikeCnt() {
+        strikeCnt++;
+    }
 }
--- codelab/java-console/src/oop/domain/User.java
+++ codelab/java-console/src/oop/domain/User.java
@@ -1,4 +1,13 @@
 package oop.domain;
 
 public class User {
+    private int[] userInputNumbers;
+
+    public int[] getUserInputNumbers() {
+        return userInputNumbers;
+    }
+
+    public void createNumbers(int[] userInputNumbers) {
+        this.userInputNumbers = userInputNumbers;
+    }
 }
